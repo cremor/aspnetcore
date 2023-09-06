7891300553c2a52c1c60d40b91fb934c6119c04c
jacalvar <jacalvar@microsoft.com>
2023-09-06 15:43:07 +0200

    Supress interactive rendering on error scenarios

--- src/Components/Endpoints/src/Rendering/EndpointHtmlRenderer.Streaming.cs
+++ src/Components/Endpoints/src/Rendering/EndpointHtmlRenderer.Streaming.cs
@@ -20,6 +20,7 @@ internal partial class EndpointHtmlRenderer
     private TextWriter? _streamingUpdatesWriter;
     private HashSet<int>? _visitedComponentIdsInCurrentStreamingBatch;
     private string? _ssrFramingCommentMarkup;
+    private bool _allowBoundaryMarkers;
 
     public void InitializeStreamingRenderingFraming(HttpContext httpContext)
     {
@@ -190,7 +191,7 @@ internal partial class EndpointHtmlRenderer
     {
         var componentId = componentFrame.ComponentId;
         var sequenceAndKey = new SequenceAndKey(componentFrame.Sequence, componentFrame.ComponentKey);
-        WriteComponentHtml(componentId, output, allowBoundaryMarkers: true, sequenceAndKey);
+        WriteComponentHtml(componentId, output, _allowBoundaryMarkers, sequenceAndKey);
     }
 
     private void WriteComponentHtml(int componentId, TextWriter output, bool allowBoundaryMarkers, SequenceAndKey sequenceAndKey = default)
